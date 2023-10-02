f620281d7310ccb294d6b5821f94240a43f0a8c7
Louis Oporto <louisoporto042@csu.fullerton.edu>
2023-10-01 22:16:18 -0700

    Removed eval

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+let x = 0.0;
+let y = 0.0;
+let operation = "";
+
 function Press(event) {
     if(event.key == '0' || event.key == '1' || event.key == '2' || 
        event.key == '3' || event.key == '4' || event.key == '5' ||
        event.key == '6' || event.key == '7' || event.key == '8' ||
-       event.key == '9' || event.key == '+' || event.key == '-' ||
-       event.key == '*' || event.key == '/')
-        document.getElementById("result").value += event.key;
+       event.key == '9') document.getElementById("result").value += event.key;
+    if(event.key == '+' || event.key == '-' || event.key == '*' || event.key == '/'){
+        x = parseFloat(document.getElementById("result").value);
+        operation = event.key;
+        document.getElementById("result").value = "";
+    }
+    if(event.key == 'Enter'){ 
+        Solve();
+        return;
+    }
 }
 
 document.querySelectorAll("button").forEach(button => {
@@ -12,6 +23,7 @@ document.querySelectorAll("button").forEach(button => {
         Click(button.innerHTML);
     });
 });
+
 
 function Click(button) {
     if(button == 'C'){
@@ -22,11 +34,21 @@ function Click(button) {
         Solve();
         return;
     }
-    document.getElementById("result").value += button;
+    if(button == '+' || button == '-' || button == '*' || button == '/'){
+        x = parseFloat(document.getElementById("result").value);
+        operation = button;
+        document.getElementById("result").value = "";
+    }
+    else{
+        document.getElementById("result").value += button;
+        y = parseFloat(document.getElementById("result").value);
+    }
 }
 
 function Solve() {
-    let x = document.getElementById("result").value;
-    let y = eval(x);
-    document.getElementById("result").value = y;
+    if(operation == '*') x = x * y;
+    if(operation == '/') x = x / y;
+    if(operation == '+') x = x + y;
+    if(operation == '-') x = x - y;
+    document.getElementById("result").value = x;
 }
